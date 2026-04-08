b7873e073d8d96fc39758fafcf9e05f36e19d784
Mac <youngdero3376@gmail.com>
2026-04-08 17:07:35 +0100

    update

--- app/Jobs/CleanAudioJob.php
+++ app/Jobs/CleanAudioJob.php
@@ -48,12 +48,12 @@ class CleanAudioJob implements ShouldQueue
             Storage::put($cleanedPath, $response->body());
 
             $localOutput = Storage::path($cleanedPath);
-            $s3Path = "voiceovers/{$this->jobId}.mp3";
+            $s3Path = "cleaned/{$this->jobId}.mp3";
 
             Storage::disk('s3')->put(
                 $s3Path,
                 file_get_contents($localOutput),
-                'private'
+                'public'
             );
 
             @unlink($localOutput);
